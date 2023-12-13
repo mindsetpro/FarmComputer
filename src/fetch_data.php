050efb814ce8005ad80300930144f6a30cf932bf
Assasian.Nator <138173273+mindsetpro@users.noreply.github.com>
2023-12-13 08:28:18 -0500

    Create fetch_data.php

---
+++ src/fetch_data.php
@@ -0,0 +1,31 @@
+<?php
+// Fetch data from Discord API
+function fetchDataFromDiscord() {
+    $botToken = getenv('TOKEN');
+
+    if (!$botToken) {
+        die('Discord bot token not provided.');
+    }
+
+    $url = 'https://discord.com/api/guilds/YOUR_GUILD_ID/members'; // Replace YOUR_GUILD_ID
+
+    $headers = [
+        'Authorization: Bot ' . $botToken,
+    ];
+
+    $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    $response = curl_exec($ch);
+    curl_close($ch);
+
+    return json_decode($response, true);
+}
+
+// Get the data
+$discordData = fetchDataFromDiscord();
+
+// Output data as JSON
+header('Content-Type: application/json');
+echo json_encode($discordData);
+?>
